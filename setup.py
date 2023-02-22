2e60363866d1c291d4b1b66dab23ed45c98aa6df
SoumitSarkar <sayhi2soumit@gmail.com>
2023-02-22 21:14:14 +0530

    fourth commit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 
 REPO_NAME = "videoIpynbRenderer"
@@ -23,11 +23,10 @@ setuptools.setup(
     long_description=long_description,
     long_description_content="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    project_urls = {
+    project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues"
     },
     package_dir={"": "src"},
-    
     # to find the package src\ipynbRenderer directory
-    packages=setuptools.find_packages(where="src")
-    )
+    packages=setuptools.find_packages(where="src"),
+)
